5dcc7e0d3876def9af67738d275a74639d062c82
Senkey <lera.leaf@gmail.com>
2021-07-07 22:00:20 +0300

    File in repository

---
+++ ToDo001/ToDo001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDo001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
